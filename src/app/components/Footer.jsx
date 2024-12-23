5c54a203303422a846b0cb092e05fe986f67088f
Amaan9838 <amaanmohd115@gmail.com>
2024-12-23 11:28:30 +0530

    update

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -5,7 +5,7 @@ const footerLinks = {
   leftSection: [
     { text: "Privacy Policy", href: "#" },
     { text: "Terms", href: "#" },
-    { text: "sales@pinova.in", href: "mailto:sales@pinova.in" }
+    { text: "hello@pinova.in", href: "mailto:hello@pinova.in" }
   ],
   rightSection: [
     { text: "Our services", href: "#services" },
@@ -22,7 +22,7 @@ const footerLinks = {
         <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
       </svg>
     ), 
-    href: "#", 
+    href: "https://x.com/pinovastudio", 
     label: "X" 
   },
   { 
